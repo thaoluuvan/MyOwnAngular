4a4afc2d08ee3584568fb131879fc2f7610c56c4
DESKTOP-GGTATVP\thaol <quynhnga@naxam.net>
2018-12-19 22:12:05 +0700

    Adding Posts List component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsCreateComponent,
+    PostsListComponent,
+
   ],
   imports: [
     BrowserModule,
